f131af48fb04b86d8e94e828b1a03f0433b8050d
Ashley Fernandes <ashleefernandes@gmail.com>
2018-05-24 23:58:32 +0530

    dynamic  composition

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,19 @@
 const contentNode = document.getElementById('contents');
 
+const issues = [
+{
+id: 1, status: 'Open', owner: 'Ravan',
+created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+title: 'Error in console when clicking Add',
+},
+{
+id: 2, status: 'Assigned', owner: 'Eddie',
+created: new Date('2016-08-16'), effort: 14,
+completionDate: new Date('2016-08-30'),
+title: 'Missing bottom border on panel',
+},
+];
+
 class IssueFilter extends React.Component {
   render() {
     return (
